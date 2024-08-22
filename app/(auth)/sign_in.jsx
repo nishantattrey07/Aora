80ba506c58e2bdb91d0f0a5a064ab2ca70befad4
nishantattrey07 <nishantattrey@gmail.com>
2024-08-22 19:28:07 +0530

    Completed Sign_In page and connected it to Sign_up page. Also made a custom component called FormField

--- app/(auth)/sign_in.jsx
+++ app/(auth)/sign_in.jsx
@@ -1,39 +1,71 @@
-import { StyleSheet, Text, View, ScrollView,Image } from "react-native";
+import { StyleSheet, Text, View, ScrollView,Image,TouchableOpacity } from "react-native";
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
+import { Redirect, router } from "expo-router";
 const Sign_In = () => {
   const [form, setForm] = useState({
     email: '',
     password: ''
   })
   return (
-    <SafeAreaView className="bg-primary  h-full"> 
+    <SafeAreaView className="bg-primary  h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="justify-center items-left px-4">
-          
           <Image
             source={images.logo}
             className="w-[115px] h-[35px]"
             resizeMode="contain"
           />
 
-          <Text className="mt-7 text-white text-2xl font-psemibold "> Sign in</Text>
+          <Text className="mt-9 mb-4 text-white text-2xl font-psemibold ">
+            {" "}
+            Sign in
+          </Text>
           <FormField
-            title='Email'
+            title="Email"
             value={form.email}
             handleChangeText={(e) => setForm({ ...form, email: e })}
+            placeholder="mypersonalemail@aora.com"
             otherStyles="mt-7"
             keyboardType="email-address"
           />
 
-        </View>
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            placeholder="mypass 🤫"
+            otherStyles="mt-5"
+            keyboardType="password"
+          />
+          <TouchableOpacity onPress={() => console.log("forgot password")}>
+            <Text className="text-right text-gray-400 mt-3 align-bottom">
+              Forgot Password
+            </Text>
+          </TouchableOpacity>
+          <CustomButton
+            title="Log In"
+            textStyles="font-pmedium text-lg"
+            containerStyle="mt-10"
+          />
 
+          <View className="flex-row justify-center items-center w-full mt-8">
+            <Text className="text-white">Don't have an account? </Text>
+            <TouchableOpacity
+              onPress={() => {
+                router.push("/sign_up")
+              }}
+            >
+              <Text className="text-secondary font-psemibold">Signup</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 export default Sign_In
